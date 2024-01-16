0b9d719b2859fe3b57f397f0a2657a7e1c60093e
meetpopat03 <81872473+meetpopat03@users.noreply.github.com>
2024-01-16 17:44:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,10 +8,16 @@ import categoryRoutes from './routes/categoryRoutes.js'
 import productRoutes from './routes/productRoutes.js'
 import cors from "cors"
 import path from "path"
+import {fileURLToPath} from "url"
 dotenv.config();
 
 // database config
 connectDB();
+
+//esmodulefix
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename);
+
 
 // REST OBJECT
 const app = express();
